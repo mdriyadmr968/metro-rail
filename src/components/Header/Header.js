6f1813367b5a4a2acc313a7a0c36c055764ddc52
mdriyadmr968 <mdriyadmr968@gmail.com>
2021-09-26 21:21:22 +0600

    test3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = () => {
     return (
       <div className="header">
-        <h2 style={{ textAlign: "center" }}>
+        <h2 style={{ textAlign: "center", color: "	#008b8b" }}>
           welcome to Gurondaz metro-rail service
         </h2>
-        <p style={{ textAlign: "center" }}>
+        <p style={{ textAlign: "center", color:"white" }}>
           explore Bangladesh with our first class Metro rail
         </p>
-        <h3 style={{ textAlign: "center" }}>
+        <h3 style={{ textAlign: "center", color:"yellow" }}>
           Our Cheapest ticket price is only 2000 Taka
         </h3>
       </div>
--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -16,11 +16,11 @@ const Total = (props) => {
 
   return (
     <div>
-      <h3>Order summary</h3>
+      <h3 className="order-sum">Order summary</h3>
       <h5>Ticket Ordered : {props.total.length}</h5>
       <p>Total Cost : {totalprice} Taka</p>
       <div>
-        <h3>Selected Locations:</h3>
+        <h3 className="selected-loc">Selected Locations:</h3>
         <h3 className = "clicked">{clicked}</h3>
       </div>
     </div>
